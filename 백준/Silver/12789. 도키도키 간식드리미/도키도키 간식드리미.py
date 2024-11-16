f31286b9b9767275c0fb557329c7fffbfa252d89
Seoyoon Choi <144862626+choiseoyoon0330@users.noreply.github.com>
2024-11-16 17:00:09 +0900

    [Silver III] Title: 도키도키 간식드리미, Time: 36 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Silver/12789. 도키도키 간식드리미/도키도키 간식드리미.py
@@ -0,0 +1,18 @@
+import sys
+input = sys.stdin.readline
+
+n = int(input())
+students = list(map(int,input().split()))
+stack = []
+now = 1
+
+for student in students:
+    stack.append(student)
+    while stack and stack[-1] == now:
+        stack.pop()
+        now += 1
+        
+if stack:
+    print("Sad")
+else:
+    print("Nice")
