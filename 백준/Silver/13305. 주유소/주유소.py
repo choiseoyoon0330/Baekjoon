4754e2618b64879f6b1c50f87975420924aa5072
Seoyoon Choi <sseo9872@gmail.com>
2026-01-10 17:34:28 +0900

    [Silver III] Title: 주유소, Time: 84 ms, Memory: 42660 KB, Score: 58 point -BaekjoonHub

---
+++ 백준/Silver/13305. 주유소/주유소.py
@@ -0,0 +1,15 @@
+import sys
+input = sys.stdin.readline
+
+n = int(input())
+road = list(map(int, input().split()))
+oil = list(map(int, input().split()))
+
+result = 0
+min_oil = 10001
+for i in range(n - 1):
+    if oil[i] < min_oil:
+        min_oil = oil[i]
+    result += min_oil * road[i]
+    
+print(result)
